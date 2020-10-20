b2e42c7685fd359d93b03dd970f05eaa585af67b
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-10-20 10:19:29 +0700

    Passing method as an argument (predicate)

--- java8inaction/src/main/java/io/github/bluething/java/java8inaction/ch1/FilteringApples.java
+++ java8inaction/src/main/java/io/github/bluething/java/java8inaction/ch1/FilteringApples.java
@@ -3,6 +3,7 @@ package io.github.bluething.java.java8inaction.ch1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class FilteringApples {
 
@@ -15,10 +16,14 @@ public class FilteringApples {
         // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
         greenApples = filterGreenApple(apples);
         System.out.println(greenApples);
+        greenApples = filterApples(apples, FilteringApples::isGreenApple);
+        System.out.println(greenApples);
 
         List<Apple> heavyApples = new ArrayList<>();
         // [Apple{color='green', weight=155}]
         heavyApples = filterHeavyApples(apples);
+        System.out.println(heavyApples);
+        heavyApples = filterApples(apples, FilteringApples::isHeavyApple);
         System.out.println(heavyApples);
     }
 
@@ -37,6 +42,16 @@ public class FilteringApples {
     }
     public static boolean isHeavyApple(Apple apple) {
         return apple.getWeight() > 150;
+    }
+
+    public static List<Apple> filterApples(List<Apple> apples, Predicate<Apple> predicate) {
+        List<Apple> filteredApples = new ArrayList<>();
+        for (Apple apple : apples) {
+            if(predicate.test(apple)) {
+                filteredApples.add(apple);
+            }
+        }
+        return filteredApples;
     }
 
     public static List<Apple> filterHeavyApples(List<Apple> apples) {
